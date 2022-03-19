d4f6fdb58819fa17c9dc7d56777ae706d8a21f7c
Vivek V <vivek@cloudloom.io>
2022-03-19 12:40:46 +0530

    font update

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -5,15 +5,15 @@ export default function Document() {
     <Html>
       <Head>
         <link
-          href="//db.onlinewebfonts.com/c/6aa06a9a432b8417171f60d8351aa310?family=Torus+Notched+SemiBold"
+          href="https://db.onlinewebfonts.com/c/6aa06a9a432b8417171f60d8351aa310?family=Torus+Notched+SemiBold"
           rel="stylesheet"
           type="text/css"
         />
-        <link
+        {/* <link
           href="//db.onlinewebfonts.com/c/23a52005216dca8d403521d06834f924?family=AvenirNextW10-Medium"
           rel="stylesheet"
           type="text/css"
-        />
+        /> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
